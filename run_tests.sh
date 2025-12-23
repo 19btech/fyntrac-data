18c6e59ba2ee5d4a10a09c1c7748576fd3c92a95
rahmed1904 <rahmed@19btech.com>
2025-12-23 11:09:57 +0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # your test case names
-tests=("TestIFRS9" "TestRevenue" "TestSBO" "TestGFO" "IFRS9_Stage3" "TestEvent" "TestAsset")
+tests=("TestIFRS9" "TestRevenue" "TestSBO" "TestGFO" "IFRS9_Stage3" "TestEvent" "TestAsset" "TestSBO_Replay_M1" "TestSBO_Replay_M2")
 
 # fully qualified test class to run
 test_class="com.fyntrac.data.testdriver.ExcelTestDriver"
