f43731d7a2b1d48e86facea82273a6414b22328a
19btech <uabbas@19btech.com>
2026-05-22 11:35:17 +0500

    spring boot test configuration change

--- src/test/java/com/fyntrac/data/DataApplicationTests.java
+++ src/test/java/com/fyntrac/data/DataApplicationTests.java
@@ -3,7 +3,11 @@ package com.fyntrac.data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "memcached.host=0.0.0.0",
+    "spring.data.mongodb.host=0.0.0.0",
+    "spring.pulsar.client.service-url=pulsar://0.0.0.0:6650"
+})
 class DataApplicationTests {
 
 	@Test
